a7a2005443d279569313ee9567bc65e41cd17027
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-03 19:53:13 +0100

    install npm  chalk@4.1.2

--- notes.controller.js
+++ notes.controller.js
@@ -1,5 +1,8 @@
-const fs = require('fs/promises')
+
+
+const fs = require('fs/promises');
 const path = require('path');
+const chalk = require('chalk')
 
 const notesPath = path.join(__dirname, 'db.json');
 async function addNote(title){
@@ -10,8 +13,8 @@ async function addNote(title){
     }
     notes.push(note);
     await fs.writeFile(notesPath, JSON.stringify(notes))
+    console.log(chalk.green("new note was added"));
 }
-addNote('Test!');
 async function getNotes(){
     const notes = await fs.readFile(notesPath, {encoding: "utf-8"});
     return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
