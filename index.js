9ebfaf4bafe5a9587e11252bc281c7ab4aab3d37
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-05 22:00:36 +0100

    add script start; install nodemon

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-const yargs = require('yargs');
+const http = require('http');
+const chalk = require('chalk');
+const port = 3000;
 
-const {addNote, printNotes} = require('./notes.controller');
-
-
-yargs.command({
-    command: "add",
-    describe: "add new note to list",
-    builder: {
-        title: {
-            type: "string",
-            describe: "note title",
-            demandOption: true
-        }
-    },
-    handler ({title}) {
-        addNote(title);
-    }
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    console.log(req.url);
+    res.end('hello');
+});
+server.listen(port,()=>{
+    console.log(chalk.green(`Server hes been started`))
 })
-yargs.command({
-    command: "list",
-    describe: "prints all notes",
-    async handler () {
-        await printNotes();
-    }
-})
-yargs.parse();
