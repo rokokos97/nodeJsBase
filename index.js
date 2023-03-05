6c986919606fc9f3be44622598f84612ae1b42e1
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-05 22:11:00 +0100

    add script serve

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     console.log(req.method);
     console.log(req.url);
-    res.end('hello');
+    res.end('hello!!!!');
 });
 server.listen(port,()=>{
     console.log(chalk.green(`Server hes been started`))
