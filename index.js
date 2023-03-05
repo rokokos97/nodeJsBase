e85c709d41cac906b6f966afe7d19ec162e82db6
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-05 22:50:37 +0100

    index.js => add request to server and display response (7-11);

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const chalk = require('chalk');
+const fs = require('fs/promises');
+const path = require('path');
 const port = 3000;
-
-const server = http.createServer((req, res) => {
-    console.log(req.method);
-    console.log(req.url);
-    res.end('hello!!!!');
+const basePath = path.join(__dirname, "pages")
+const server = http.createServer(async (req, res) => {
+    if (req.method === "GET"){
+        const content = await fs.readFile(path.join(basePath, "index.html"));
+        res.end(content);
+    }
 });
 server.listen(port,()=>{
     console.log(chalk.green(`Server hes been started`))
