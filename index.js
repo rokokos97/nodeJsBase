4a4913dfeb0a42074ab68b69a8ada5f1b7d8bbf1
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-05 13:05:30 +0100

    create server; import chalk; start server;

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const http = require('http');
+const chalk = require('chalk')
+const port = 3000;
+const server = http.createServer(() => {});
 
+server.listen(port, ()=>{
+    console.log(chalk.green(`Server has been started on port ${port}...`))
+});
