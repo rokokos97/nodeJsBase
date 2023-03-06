48b2dc897b59802f2251e458c10ba25eae60770b
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-06 19:10:10 +0100

    add button edit

--- public/app.js
+++ public/app.js
@@ -4,9 +4,20 @@ document.addEventListener('click', event => {
         remove(event.target.dataset.id).then(()=>{
             event.target.closest('li').remove()
         })
+    } else if (event.target.dataset.type === 'edit'){
+        const newTitle = prompt('Enter new title')
+        if(newTitle){
+            edit(event.target.dataset.id, newTitle)
+        }
+        console.log(event.target.dataset.id)
     }
+
 })
 
 async function remove(id){
     await fetch(`/${id}`, {method: 'DELETE'})
+}
+async function edit(id , title){
+    await fetch(`/${id}`,{method:'PUT', body: title})
+    console.log(id,title)
 }
