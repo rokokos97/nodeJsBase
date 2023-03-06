fcbfa8b1eba0d59c71918465069bc48b843e5a0b
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-06 15:03:09 +0100

    install express

---
+++ server.exemple.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const chalk = require('chalk');
+const fs = require('fs/promises');
+const path = require('path');
+const port = 3000;
+const basePath = path.join(__dirname, "pages")
+const {addNote} = require('./notes.controller')
+
+const server = http.createServer(async (req, res) => {
+    if (req.method === "GET"){
+        const content = await fs.readFile(path.join(basePath, "index.html"));
+        res.writeHead(200, {
+            'Content-Type':'text/html'
+        })
+        res.end(content);
+    } else if (req.method === "POST"){
+        const body = [];
+        res.writeHead( 200, {
+            'Content-Type': 'text/plain, charset=utf-8'
+        })
+        req.on('data', data => {
+            // console.log('data', data)
+            body.push(Buffer.from(data))
+        })
+        req.on("end", ()=>{
+            const title = body.toString().split("=")[1].replaceAll("+"," ")
+            console.log('end', body.toString().split("=")[1].replaceAll("+"," "))
+            addNote(title);
+            res.end(`Title = ${title}`)
+        })
+
+
+    }
+});
+server.listen(port,()=>{
+    console.log(chalk.green(`Server hes been started`))
+})
