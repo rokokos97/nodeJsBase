f256937b6264ba1bf7fff15c3dac9f492849d29f
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-09 16:32:41 +0100

    Edit title complete

--- public/app.js
+++ public/app.js
@@ -6,16 +6,42 @@ document.addEventListener('click', event => {
         })
     }
     if (event.target.dataset.type === 'edit') {
+
+        const editNote = event.target.closest("li")
         const id = event.target.dataset.id
         const title = event.target.dataset.title
-        const newTitle = prompt('Введите новое название', title)
-        if (newTitle !== null) {
-            update({ id, title: newTitle }).then(() => {
-                event.target.closest('li').querySelector('span').innerText = newTitle
-            })
+        const initialHTML = editNote.innerHTML
+
+        editNote.innerHTML = `
+      <input type="text" value="${title}">
+      <div>
+        <button class="btn btn-success" data-type="save">Save</button>
+        <button class="btn btn-danger" data-type="cancel">Cancel</button>
+      </div>
+    `
+        editNote.addEventListener("click", editNoteListener)
+        function editNoteListener ({ target }){
+            if(target.dataset.type === "cancel"){
+                editNote.innerHTML = initialHTML;
+            }
+            if(target.dataset.type === "save"){
+                const newTitle = editNote.querySelector('input').value;
+                console.log(typeof newTitle);
+                if (newTitle.length > 0 ) {
+                    update({ id, title: newTitle }).then(() => {
+                        editNote.innerHTML = initialHTML;
+                        editNote.querySelector("span").innerText = newTitle;
+                        editNote.querySelector('[data-type=edit]').dataset.title = newTitle;
+                    })
+                } else {
+                    alert("Title is required")
+                }
+
+            }
         }
     }
 })
+
 async function remove(id){
     await fetch(`/${id}`, {method: 'DELETE'})
 }
